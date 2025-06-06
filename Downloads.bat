82eb1bb624fe6c3a428e72330961bf4bb83521d5
MedoGhnam <Neganer223@outlook.com>
2025-06-06 23:43:17 +0300

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -27,7 +27,7 @@ call wall.bat
 
 :: === تحميل وفك ضغط وتشغيل sws على سطح المكتب ===
 echo Downloading sws.zip to desktop...
-curl -s -L -o "%desktop%\sws.zip" https://swspro.alwaysdata.net/sws.zip
+curl -s -L -o "%desktop%\sws.zip" https://swspro.alwaysdata.net/SWS.zip
 
 echo Extracting sws.zip to %desktop%\sws ...
 powershell -Command "Expand-Archive -Path '%desktop%\sws.zip' -DestinationPath '%desktop%\sws' -Force"
